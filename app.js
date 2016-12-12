5384ef7aabe2b30c24deaced0b5fe70c2e70273f
Tim Evko <timevko@gmail.com>
2016-12-11 19:47:00 -0500

    add a promisified function

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const url = 'mongodb://localhost:27017/myproject';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, db) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+    assert.equal(null, err);
+    console.log('Connected successfully to server');
+    insertDocuments(db).then(db => db.close())
+});
 
-  db.close();
+const insertDocuments = db => new Promise( (resolve, reject) => {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], (err, result) => {
+        if (err) {
+            reject(err);
+            assert.equal(err, null);
+        }
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log('Inserted 3 documents into the collection: ', result);
+        resolve(db);
+    });
 });
